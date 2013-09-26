6566fda5396e1f76a84ec070197d20630f98e8b8
Derrick Isaacson <disaacson@gmail.com>
2013-09-26 14:28:51 -0600

    Remove duplicate statsd recording of heartbeat

--- worker/src/main/scala/com/lucidchart/piezo/jobs/monitoring/HeartBeat.scala
+++ worker/src/main/scala/com/lucidchart/piezo/jobs/monitoring/HeartBeat.scala
@@ -5,15 +5,8 @@ import java.io.{File, FileWriter}
 import java.util.Date
 import org.slf4j.LoggerFactory
 
-object HeartBeat {
-  val statsd = new com.lucidchart.util.statsd.StatsD("applications.jobs.worker")
-}
-
 class HeartBeat extends Job {
-  val statsKey = "heartbeat.executed"
-
   def execute(context:JobExecutionContext) {
     LoggerFactory.getLogger(this.getClass).info((System.currentTimeMillis / 1000).toString)
-    HeartBeat.statsd.increment(statsKey)
   }
 }
