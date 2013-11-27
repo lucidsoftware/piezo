21c39cf85d59b465b148995068f5316e093aef61
Jeff Wright <jeff@jeff.office.lucidchart.com>
2013-11-27 14:17:50 -0700

    added a catchall error page and a not found page

---
+++ admin/app/Global.scala
@@ -0,0 +1,24 @@
+import play.api._
+import play.api.mvc._
+import play.api.mvc.Results._
+import play.api.Play.current
+
+object Global extends GlobalSettings
+{
+  val logger = Logger("com.lucidchart.piezo.Global")
+
+  override def onHandlerNotFound(request: RequestHeader): Result = {
+    logger.error("Request handler not found for URL: " + request.uri)
+    NotFound(com.lucidchart.piezo.admin.views.html.errors.notfound(None)(request))
+  }
+
+  override def onError(request: RequestHeader, ex: Throwable) = {
+    logger.error("Error handling request for URL: " + request.uri, ex)
+    if(play.api.Play.isProd || play.api.Play.isTest) {
+      InternalServerError(com.lucidchart.piezo.admin.views.html.errors.error(Option(ex.getMessage))(request))
+    } else {
+      super.onError(request, ex)
+    }
+  }
+
+}
