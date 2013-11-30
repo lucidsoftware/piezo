995857334396d9c6077d8c065feb243eb10c0900
Derrick Isaacson <disaacson@gmail.com>
2013-11-29 23:23:53 -0700

    Don't try to read datasource parameters if datasource name does not exist in quartz properties. Code review pending by Kesler.

--- worker/src/main/scala/com/lucidchart/piezo/ConnectionProvider.scala
+++ worker/src/main/scala/com/lucidchart/piezo/ConnectionProvider.scala
@@ -6,15 +6,21 @@ import org.slf4j.LoggerFactory
 import java.io._
 
 class ConnectionProvider(props: Properties) {
+  val logger = LoggerFactory.getLogger(this.getClass)
   private val dataSource = props.getProperty("org.quartz.jobStore.dataSource")
-  private val provider = new PoolingConnectionProvider(
-    props.getProperty("org.quartz.dataSource." + dataSource + ".driver"),
-    props.getProperty("org.quartz.dataSource." + dataSource + ".URL"),
-    props.getProperty("org.quartz.dataSource." + dataSource + ".user"),
-    props.getProperty("org.quartz.dataSource." + dataSource + ".password"),
-    props.getProperty("org.quartz.dataSource." + dataSource + ".maxConnections").toInt,
-    props.getProperty("org.quartz.dataSource." + dataSource + ".validationQuery")
-  )
+  private val provider = if(dataSource != null) {
+    Some(new PoolingConnectionProvider(
+      props.getProperty("org.quartz.dataSource." + dataSource + ".driver"),
+      props.getProperty("org.quartz.dataSource." + dataSource + ".URL"),
+      props.getProperty("org.quartz.dataSource." + dataSource + ".user"),
+      props.getProperty("org.quartz.dataSource." + dataSource + ".password"),
+      props.getProperty("org.quartz.dataSource." + dataSource + ".maxConnections").toInt,
+      props.getProperty("org.quartz.dataSource." + dataSource + ".validationQuery")
+    ))
+  } else {
+    logger.info("No job store found in config")
+    None
+  }
 
-  def getConnection = provider.getConnection
+  def getConnection = provider.get.getConnection
 }
