e7919a18d87204c2bae256574177e544e4db3148
Derrick Isaacson <disaacson@gmail.com>
2013-11-30 23:03:58 -0700

    Add run.sh for admin

---
+++ admin/bin/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DEPS_DIR="$SOURCE_DIR/../target/staged"
+QUARTZ_PROPS_FILE="$SOURCE_DIR/../../worker/src/main/resources/quartz.properties"
+
+java -Dorg.quartz.properties=$QUARTZ_PROPS_FILE -Dpidfile.path=/tmp/pid -Dnetworkaddress.cache.ttl=10 -Dnetworkaddress.cache.negative.ttl=10 -Dhttp.port=8001 -cp "${DEPS_DIR}/*" play.core.server.NettyServer
