c7c10f488f138cce797637744ef9893f9a36c7b7
Ken Reese <ken@lucidchart.com>
2016-05-02 10:07:32 -0600

    Fix for oversized indexes with UTF8mb4 encoding

--- worker/src/main/resources/create_history_tables.sql
+++ worker/src/main/resources/create_history_tables.sql
@@ -1,9 +1,9 @@
 CREATE TABLE job_history(
     fire_instance_id VARCHAR(120),
-    job_name VARCHAR(200) NOT NULL,
-    job_group VARCHAR(200) NOT NULL,
-    trigger_name VARCHAR(200) NOT NULL,
-    trigger_group VARCHAR(200) NOT NULL,
+    job_name VARCHAR(190) NOT NULL,
+    job_group VARCHAR(190) NOT NULL,
+    trigger_name VARCHAR(190) NOT NULL,
+    trigger_group VARCHAR(190) NOT NULL,
     success BOOLEAN NOT NULL,
     start DATETIME NOT NULL,
     finish DATETIME,
@@ -13,8 +13,8 @@ CREATE TABLE job_history(
 );
 
 CREATE TABLE trigger_history(
-    trigger_name VARCHAR(200) NOT NULL,
-    trigger_group VARCHAR(200) NOT NULL,
+    trigger_name VARCHAR(190) NOT NULL,
+    trigger_group VARCHAR(190) NOT NULL,
     scheduled_start DATETIME NOT NULL,
     actual_start DATETIME,
     finish DATETIME NOT NULL,
