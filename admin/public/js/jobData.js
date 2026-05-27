cfbc9295b56fe6069b3127bc4b9f6d081e83d24b
Thayne McCombs <thayne@lucidchart.com>
2026-05-27 14:50:26 -0600

    refactor: remove jquery from jobdata.js
    Use raw js instead. Also refactor the html a little bit to make it
    easier to manipulate components.

--- admin/public/js/jobData.js
+++ admin/public/js/jobData.js
@@ -1,44 +1,51 @@
 (function () {
-    var fixDataMapIndexes = function() {
-        $('[id*=job-data-map]').each(function(i, element){
+    function fixDataMapIndexes() {
+        const inputs = document.querySelectorAll('input[id*=job-data-map_]');
+        for (let i = 0; i < inputs.length; i++) {
+            const element = inputs[i];
             if (i % 2 === 0) {
-                var itemNumber = i / 2;
+                const itemNumber = i / 2;
                 element.id = 'job-data-map_' + itemNumber + '_key';
                 element.name = 'job-data-map[' + itemNumber + '].key';
-            }
-
-            if (i % 2 !== 0) {
-                var itemNumber = (i - 1) / 2;
+            } else {
+                const itemNumber = (i - 1) / 2;
                 element.id = 'job-data-map_' + itemNumber + '_value';
                 element.name = 'job-data-map[' + itemNumber + '].value';
             }
-        })
-    };
+        }
+    }
 
-    $('.job-data-map').on('click', '.job-data-delete a', function () {
-        $(this).parent().next().next().remove();
-        $(this).parent().next().remove();
-        $(this).parent().remove();
+    document.getElementById('job-data-map')?.addEventListener('click', function(e) {
+        // Only act on clicks on the delete link
+        const anchor = e.target.closest('a.job-data-delete');
+        if (anchor) {
+            e.preventDefault();
+            const entry = anchor.closest('.job-data-entry');
+            entry?.remove();
+            fixDataMapIndexes();
+        }
+    });
+
+    document.getElementById('job-data-add')?.addEventListener('click', function() {
+        const prevEntry = this.previousElementSibling;
+
+        // clone before we add the delete button, or we end up with duplicate delete buttons
+        const entry = prevEntry.cloneNode(true);
+
+        prevEntry.insertAdjacentHTML('afterbegin', '<a class="job-data-delete text-right" href="#">delete</a>');
+
+        // reset inputs
+        const inputs = entry.getElementsByTagName('input');
+        for (const input of inputs) {
+            input.value = '';
+        }
+
+        this.before(entry);
 
         fixDataMapIndexes();
     });
 
-    $('.job-data-add').click(function () {
-        var key = $(this).prev().prev().clone();
-        var value = $(this).prev().clone();
-
-        $(this).prev().prev().before($('<div class="job-data-delete text-right"><a href="#">delete</a></div>'));
-
-        key.find('input').val('');
-        value.find('input').val('');
-
-        $(this).before(key);
-        $(this).before(value);
-
-        fixDataMapIndexes();
-    });
-
-    $('form').submit(function () {
+    document.querySelector('form')?.addEventListener('submit', function() {
         fixDataMapIndexes();
     });
 })();
