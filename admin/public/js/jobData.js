0c82f415a747d3c53889499f21116bd1a0da217a
Thayne McCombs <thayne@lucidchart.com>
2026-05-29 14:14:32 -0600

    refactor: Get rid of jquery
    This upgrades bootstrap and removes the rest of the jquery code, and finally removes
    the vendored jquery dependency itself.
    
    This has some minor changes to the appearance of the interface, but the functionality should be the
    same.

--- admin/public/js/jobData.js
+++ admin/public/js/jobData.js
@@ -17,7 +17,7 @@
 
     document.getElementById('job-data-map')?.addEventListener('click', function(e) {
         // Only act on clicks on the delete link
-        const anchor = e.target.closest('a.job-data-delete');
+        const anchor = e.target.closest('.job-data-delete');
         if (anchor) {
             e.preventDefault();
             const entry = anchor.closest('.job-data-entry');
@@ -27,12 +27,13 @@
     });
 
     document.getElementById('job-data-add')?.addEventListener('click', function() {
-        const prevEntry = this.previousElementSibling;
+        const parent = this.parentElement;
+        const prevEntry = parent.previousElementSibling;
 
         // clone before we add the delete button, or we end up with duplicate delete buttons
         const entry = prevEntry.cloneNode(true);
 
-        prevEntry.insertAdjacentHTML('afterbegin', '<a class="job-data-delete text-right" href="#">delete</a>');
+        prevEntry.querySelector('.job-data-delete').classList.add('shown');
 
         // reset inputs
         const inputs = entry.getElementsByTagName('input');
@@ -40,7 +41,7 @@
             input.value = '';
         }
 
-        this.before(entry);
+        parent.before(entry);
 
         fixDataMapIndexes();
     });
