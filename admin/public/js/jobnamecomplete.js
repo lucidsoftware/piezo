6a944bc63800fd279011331d71b1d6157e6e7d66
Thayne McCombs <thayne@lucidchart.com>
2026-06-04 14:50:40 -0600

    fix: Add null check
    Make sure that we only update the job-names-list if it exists.

--- admin/public/js/jobnamecomplete.js
+++ admin/public/js/jobnamecomplete.js
@@ -6,7 +6,7 @@
 
         const datalist = document.getElementById('job-names-list');
 
-        if (jobGroup == currentJobGroup) {
+        if (!datalist || jobGroup == currentJobGroup) {
             return;
         }
 
